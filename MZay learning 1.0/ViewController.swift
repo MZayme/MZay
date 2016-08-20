283cb7bb92d7802f6fdd04a42202f40f426ab8b7
Савченко Никита Александрович <savchenkonikita@gmail.com>
2016-08-21 00:11:40 +0300

    Initial Commit

---
+++ MZay learning 1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MZay learning 1.0
+//
+//  Created by Nikita Savchenko on 8/21/16.
+//  Copyright © 2016 Nikita Savchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MZay learning 1.0Tests/MZay_learning_1_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  MZay_learning_1_0Tests.swift
+//  MZay learning 1.0Tests
+//
+//  Created by Nikita Savchenko on 8/21/16.
+//  Copyright © 2016 Nikita Savchenko. All rights reserved.
+//
+
+import XCTest
+@testable import MZay_learning_1_0
+
+class MZay_learning_1_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
